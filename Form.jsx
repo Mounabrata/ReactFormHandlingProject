a7869433fb79837ad043926d221ed48d8c78df42
Mounabrata Baidya <122028475+Mounabrata@users.noreply.github.com>
2025-05-03 16:17:11 +0530

    Add files via upload

---
+++ Form.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function Form({ handleFormSubmitData }) {
+  const { register, handleSubmit, reset } = useForm();
+  const handleFormSubmit = (data) => {
+    handleFormSubmitData(data);
+    reset();
+  };
+  return (
+    <div className="mt-10 gap-10 flex justify-center ">
+      <form className="flex gap-10" onSubmit={handleSubmit(handleFormSubmit)}>
+        <input
+          {...register("name")}
+          className="rounded-md px-2 py-1 text-base font-semibold outline-none"
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          {...register("email")}
+          className="rounded-md px-2 py-1 text-base font-semibold outline-none"
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          {...register("image")}
+          className="rounded-md px-2 py-1 text-base font-semibold outline-none"
+          type="text"
+          placeholder="Image URL"
+        />
+        <input
+          className="rounded-md px-5 py-1 bg-blue-500 text-white font-semibold"
+          type="Submit"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
